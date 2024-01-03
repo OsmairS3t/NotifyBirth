10298246b47ecf5246935e8fbb0bc55a3022a0b6
osmair <osmair144@gmail.com>
2024-01-03 15:57:43 -0300

    Initial System

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Platform  } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  // async function handleCallNotification() {
+  //   const {status} = await Notifications.getPermissionsAsync();
+  //   if (status !== 'granted'){
+  //     alert('Você não tem permissão para receber notificações.')
+  //     return;
+  //   }
+  //   let token = (await Notifications.getExpoPushTokenAsync()).data
+  //   console.log(token)
+  // }
+
+ const handleCallNotification = () => Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'BirthDay',
+      body: "Today is my Day!",
+    },
+    trigger: null,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Notify BirthDay</Text>
+      <Button title='Notify Me' onPress={handleCallNotification} />
       <StatusBar style="auto" />
     </View>
   );
